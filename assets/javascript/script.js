fa6ec157a4c4bcffbc5826bdbd45d34c7f128a4e
Andrew Weiss <wandrew8@gmail.com>
2019-09-10 19:12:54 -0700

    Wells added to news articles and JS bugs worked out

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,42 +1,58 @@
 
 
 
-function grabNYTimesArticles(searchQuery, numberOfRecords, beginDate, endDate){
+function grabNYTimesArticles(searchQuery, numberOfRecords, beginDate, endDate) {
     console.log('grabbing new york times articles');
-    
+
     var queryURL;
     if (beginDate !== '' && endDate !== '') {
 
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
     }
     else if (beginDate !== '') {
-        
+
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&begin_date=" + beginDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
     }
     else if (endDate !== '') {
-        
+
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
     }
     else {
-        
+
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
     }
-    
+
     $.ajax({
         url: queryURL,
-    }).then(function(data){
+    }).then(function (data) {
+
+        console.log(data.response)
 
         for (var i = 0; i < numberOfRecords; i++) {
             var url = data.response.docs[i].web_url;
-            var snippet = data.response.docs[i].snippet;
+            var headline = data.response.docs[i].headline.print_headline;
+            var snippet = data.response.docs[i].lead_paragraph;
 
-            $('#results').append(`<p><a href="${url}">${snippet}</a></p>`);
+            if (snippet == null) {
+                var snippet = data.response.docs[i].snippet;
+            } var snippet = data.response.docs[i].lead_paragraph;
+
+
+
+            if (headline == null) {
+                var headline = data.response.docs[i].headline.main;
+            } else {
+                var headline = data.response.docs[i].headline.print_headline;
+            }
+
+            $('#results').append(`<div id="newsDiv" class="well"><h2><a href="${url}">${headline}</h2></a><br><p class='well bg-light'>${snippet}</p></div>`);
         }
     });
 }
 
-$('#searchButton').on('click', function(event){
+$('#searchButton').on('click', function (event) {
     event.preventDefault();
+    $('#results').empty();
 
     var searchQueryToUse = $('#searchInput').val();
     var startYear = $('#startYear').val();
@@ -46,8 +62,9 @@ $('#searchButton').on('click', function(event){
     grabNYTimesArticles(searchQueryToUse, resultsMax, startYear, endYear);
 });
 
-$('#clearButton').on('click', function(event){
+$('#clearButton').on('click', function (event) {
     event.preventDefault();
 
     $("#clearButton").empty();
 })
+
